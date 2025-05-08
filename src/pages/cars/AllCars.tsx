ace3fa93d9d4341abdf31ea7c2395fa7b7c71e2c
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2025-05-09 03:12:08 +0400

    update: search and filter functionality in all cars page

--- src/pages/cars/AllCars.tsx
+++ src/pages/cars/AllCars.tsx
@@ -6,14 +6,12 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { IoSearchSharp } from "react-icons/io5";
 import { Button } from "@/components/ui/button";
+import { RxCrossCircled } from "react-icons/rx";
+
 import Skeleton from "@/components/Skeleton";
-
-
-
-
 const AllCars = () => {
   const navigate = useNavigate();
-  const { register, handleSubmit: handleSearchSubmit } = useForm();
+  const { register, handleSubmit: handleSearchSubmit , setValue} = useForm();
   const searchParams = new URLSearchParams(window.location.search);
   const searchTerm = searchParams.get("search") || "";
   const filterTerm = searchParams.get("filter") || "";
@@ -22,15 +20,21 @@ const AllCars = () => {
     filter: filterTerm || undefined,
   });
   // Get unique car brands for the filter dropdown
-  const uniqueBrands = [...new Set((data?.data as any[])?.map((car) => car.brand))];
+  const uniqueBrands = [
+    ...new Set((data?.data as any[])?.map((car) => car.brand)),
+  ];
 
   // Handle brand filter change
   const handleBrandChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(e.target.value);
     const params = new URLSearchParams(window.location.search);
-    if (e.target.value) params.set("filter", e.target.value);
-    else params.delete("filter"); // Remove filter if no brand is selected
+    if (e.target.value) {
+      params.set("filter", e.target.value);
+    } else {
+      params.delete("filter");
+    }
     navigate(`?${params.toString()}`);
-    refetch(); // Refetch data with the new query parameters
+    refetch();
   };
 
   // Handle clear filters
@@ -40,17 +44,24 @@ const AllCars = () => {
     navigate(`?${params.toString()}`);
     refetch(); // Refetch data with the new query parameters
   };
+  const handleClearSearch = () => {
+    const params = new URLSearchParams(window.location.search);
+    params.delete("search"); // Clear the filter
+    navigate(`?${params.toString()}`);
+    setValue("searchTerm", '')
+    refetch();
+  };
   const onSearchSubmit = (formData: any) => {
     const params = new URLSearchParams(window.location.search);
-    params.set('search', formData.searchTerm);
+    params.set("search", formData.searchTerm);
     navigate(`?${params.toString()}`);
     refetch(); // Refetch data with the new query parameters
   };
 
-  // console.log(data?.data)
+  console.log(searchTerm);
   return (
-    <div className="h-screen overflow-y-auto px-6 md:px-12 lg:px-20">
-      <div className="flex flex-col lg:flex-row z-[999]">
+    <div className="px-6 md:px-12 lg:px-20">
+      <div className="flex h-screen overflow-hidden flex-col lg:flex-row z-[999]">
         {/* sidebar */}
         <div className="w-full lg:w-[20%] bg-gray-100 mt-4 md:mt-0 md:p-6">
           <h3 className="font-semibold mb-5">Search & Filter</h3>
@@ -72,7 +83,7 @@ const AllCars = () => {
               <option value="" className="text-sm">
                 All Brands
               </option>
-              {uniqueBrands.map((brand:string, index:number) => (
+              {uniqueBrands.map((brand: string, index: number) => (
                 <option key={index} value={brand}>
                   {brand}
                 </option>
@@ -88,25 +99,29 @@ const AllCars = () => {
             Clear Filters
           </Button>
         </div>
-        <div className="lg:flex-1 lg:py-6 md:pl-6">
+        <div className="lg:flex-1 overflow-y-auto lg:py-6 md:pl-6">
           {/* search field  */}
           <div className="h-14 w-[60%] flex items-center justify-center mx-auto mb-6">
             <form
-              className="w-full relative lg:px-2 py-2 flex bg-white  items-center rounded-lg"
+              className="w-full relative lg:px-2 py-2 flex items-center rounded-lg"
               onSubmit={handleSearchSubmit(onSearchSubmit)}
             >
               <input
                 type="text"
                 placeholder="Search "
-                className="w-full outline-none text-sm"
+                className="w-full bg-gray-200 py-2 px-4 rounded-lg outline-none text-sm"
                 {...register("searchTerm")}
               />
-              <button
-                type="submit"
-                className="absolute text-lg  hover:text-[#D32F2F] right-[12px]"
-              >
-                <IoSearchSharp />
-              </button>
+              {searchTerm ? (
+                <RxCrossCircled onClick={() => handleClearSearch()} className="absolute text-lg cursor-pointer hover:text-[#D32F2F] right-[16px]"/>
+              ) : (
+                <button
+                  type="submit"
+                  className="absolute text-lg  hover:text-[#D32F2F] right-[16px]"
+                >
+                  <IoSearchSharp />
+                </button>
+              )}
             </form>
           </div>
           {data?.data?.length > 0 ? (
@@ -115,12 +130,25 @@ const AllCars = () => {
                 ?.slice()
                 .reverse()
                 .map((singleData: TCar) => (
-                  <ProductCard key={singleData._id} data={singleData} _id={""} brand={""} model={""} year={0} price={0} category={"Sedan"} description={""} quantity={0} inStock={false} image={""} />
+                  <ProductCard
+                    key={singleData._id}
+                    data={singleData}
+                    _id={""}
+                    brand={""}
+                    model={""}
+                    year={0}
+                    price={0}
+                    category={"Sedan"}
+                    description={""}
+                    quantity={0}
+                    inStock={false}
+                    image={""}
+                  />
                 ))}
             </div>
           ) : (
             <div className="w-full">
-              <Skeleton/>
+              <Skeleton />
             </div>
           )}
         </div>
